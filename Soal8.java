0334782eee9cbeb0302b989465ad8bff1ca2a34f
NurlianaSaid <154890093+NurlianaSaid@users.noreply.github.com>
2025-11-02 19:11:42 +0800

    Update Soal8.java

--- Soal8.java
+++ Soal8.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class hurufBalik {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Masukkan kata/kalimat: ");
+        System.out.print("Masukkan sebuah kata: ");
         String input = in.nextLine();
         StringBuilder sb = new StringBuilder(input);
         String hasil = sb.reverse().toString();
-        System.out.println("Hasil membalikkan: " + hasil);
+        System.out.println(hasil);
     }
 }
